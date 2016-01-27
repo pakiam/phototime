f8b4d3d2a22f5c85ee7d4355c7381425f2f9a66b
Maksim <akiam@mail.ru>
2016-01-27 16:02:05 +0300

    added my vision on Slider; now its in main.js need to replace it as a plugin for jQ. At least it seems working. some fixes for slider

--- js/main.js
+++ js/main.js
@@ -2,20 +2,25 @@ $(document).ready(function () {
 
     $.fn.mySlider = function (options) {
         var currentClick = 0;
-        console.log(currentClick);
+
+        //default settings
         var defaults = {
-            sliderItems: 'slider-item'
+            classForSliderItems: 'slider-item',
+            classForCurrentSlide: 'current-slide',
+            classForTotalSlides: 'total-slides'
+
         };
         var settings = $.extend({}, defaults, options);
 
+        //make HTML for slider
         $(this).each(function () {
             var slider = $(this);
-            $(slider).append("<div class='controls'> <div><p>01/" + getSliderCount() + "</p></div> <i class='fa fa-chevron-left slider-scroll-left'></i><i class='fa fa-chevron-right slider-scroll-right'></i></div>");
+            $(slider).append("<div class='controls'> <div><p><span class='" + settings.classForCurrentSlide + "'>01</span>/<span class='" + settings.classForTotalSlides + "'>" + getSliderCount() + "</span></p></div> <i class='fa fa-chevron-left slider-scroll-left'></i><i class='fa fa-chevron-right slider-scroll-right'></i></div>");
             $(slider).find('li').each(function () {
-                $(this).addClass(settings.sliderItems + $(this).index());
+                $(this).addClass(settings.classForSliderItems + $(this).index());
             });
             $(slider).find('li').first().addClass('active');
-
+            //getting total slides to show it on page
             function getSliderCount() {
                 var slides = $(slider).find('li').length;
                 if (slides > 0 && slides < 10) {
@@ -26,40 +31,47 @@ $(document).ready(function () {
                     return 'no slides';
                 }
             }
+            //main func of slider makes it move
+            function sliderAction(clickedObject) {
+                var sl = $(clickedObject).parent().parent();
+                var bl = $(sl).find('ul');
+                var gl = $('.active').innerWidth();
 
-            function getItSlide(sl) {
-                var ul = $(sl).find('ul');
-                var step = $(settings.sliderItems).outerWidth(true);
-                $(ul).animate({marginLeft: '-' + step + 'px'}, 500);
+                var step = gl * currentClick;
+                $(bl).animate({marginLeft: '-' + step + 'px'}, 500);
+                console.log(currentClick);
+                return currentSlide(currentClick);
             }
 
             $('.slider-scroll-right').on('click', function () {
+                var self = $(this);
                 currentClick++;
                 if (currentClick > getSliderCount() - 1) {
                     currentClick = 0;
                 }
-                var sl = $(this).parent().parent();
-                var bl = $(sl).find('ul');
-                var gl = $('.active').innerWidth();
-                var gavno = gl * currentClick;
-                $(bl).animate({marginLeft: '-' + gavno + 'px'}, 500);
-                console.log(currentClick);
+
+                sliderAction(self);
+
             });
             $('.slider-scroll-left').on('click', function () {
+                var self = $(this);
                 currentClick--;
-                if (currentClick <0) {
-                    currentClick = getSliderCount()-1;
+                if (currentClick < 0) {
+                    currentClick = getSliderCount() - 1;
                 }
-                var sl = $(this).parent().parent();
-                var bl = $(sl).find('ul');
-                var gl = $('.active').innerWidth();
 
-                var gavno = gl * currentClick;
-                $(bl).animate({marginLeft: '-'+ gavno + 'px'}, 500);
-                console.log(currentClick);
+                sliderAction(self);
             });
         });
+        //shows # of slide in real-time
+        function currentSlide(slideN) {
+            if (slideN < 10) {
+                return $('.' + settings.classForCurrentSlide).html('0' + (slideN + 1));
+            } else {
+                return $('.' + settings.classForCurrentSlide).html(slideN + 1);
+            }
 
+        }
     };
 
 
